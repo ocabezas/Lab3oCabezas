da509a9da35a657c16531e5c6e364c9686d6f1bb
Unknown <ocabezasc@ucenfotec.ac.cr>
2018-02-10 10:45:35 -0600

    Initial commit creación de proyecto

---
+++ Laboratorio3/Laboratorio3/Entities/Animal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio3.Entities
+{
+    public class Animal
+    {
+
+    }
+}
---
+++ Laboratorio3/Laboratorio3/Entities/Produccion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio3.Entities
+{
+    class Produccion
+    {
+        public int iD { get; set; }
+        public DateTime productionDate { get; set; }
+        public string Type { get; set; }
+        public int Quantity { get; set; }
+        public int Value { get; set; }
+        public int idAnimal { get; set; }
+
+    }
+}
